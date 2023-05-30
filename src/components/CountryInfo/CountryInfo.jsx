dc3173594ee97d6753b126d36683ca89c51144f0
Karani Wachira <wachirakeith@gmail.com>
2023-05-30 22:41:17 +0300

    worked on the challenge ui, filter & search function

---
+++ src/components/CountryInfo/CountryInfo.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { apiURL } from "../util/api";
+import { Link } from "react-router-dom";
+
+const CountryInfo = () => {
+  const [country, setCountry] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const { countryName } = useParams();
+
+
+  useEffect(() => {
+    const getCountryByName = async () => {
+      try {
+        const res = await fetch(`${apiURL}/name/${countryName}`);
+
+        if (!res.ok) throw new Error("country doesn't exit yet!");
+
+        const data = await res.json();
+
+        setCountry(data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setError(error.message);
+      }
+    };
+
+    getCountryByName();
+  }, [countryName]);
+
+  return (
+    <div className="country___info">
+     
+      {isLoading && !error && <h4>zzzzzzz...</h4>}
+      {error && !isLoading && { error }}
+
+      {country?.map((country, index) => (
+        <div className="country_container" key={index}>
+          <div className="countryinfoimage">
+            <img src={country.flags.png} alt="" />
+          </div>
+
+          <div className="countryinfo">
+            <h3>{country.name.common}</h3>
+
+            <div className="countryinfoleft">
+              <h5>
+                Population:{" "}
+                <span>
+                  {new Intl.NumberFormat().format(country.population)}
+                </span>
+              </h5>
+              <h5>
+                Region: <span>{country.region}</span>
+              </h5>
+              <h5>
+                Sub Region: <span>{country.subregion}</span>
+              </h5>
+              <h5>
+                Capital: <span>{country.capital}</span>
+              </h5>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CountryInfo;
---
+++ src/components/util/api.js
@@ -0,0 +1,5 @@
+
+
+
+// api link from rest countries
+export const apiURL = "https://restcountries.com/v3.1";
